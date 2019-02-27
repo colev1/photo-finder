ba42e9641b878ce3fdb9e2ec13fe5a4d130eec2e
Cole Vanacore <colevanacore@gmail.com>
2019-02-27 09:21:45 -0700

    Add icons to page up and back buttons

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
   <div id="app">
-      <h1 v-bind:class="{'small-header': showImages}"> PhotoFindr </h1>
+      <h1 v-bind:class="{'small-header': showImages}"> 
+        PhotoFindr 
+      </h1>
       <h4> search for a photo by keyword.. </h4>
     <form v-bind:class="{'small-form': showImages}">
       <font-awesome-icon icon="search" class="search-icon" />
-      <input type="text" v-model="keyword" placeholder="search">
+      <input type="text" v-model="keyword" placeholder="search" />
       <button v-on:click="submitSearch"> 
         search
       </button>
     </form>
-    <div class ='results-container' v-bind:class="{'hidden': showImages=== false}">
-        <p>
-      total images matching keyword: {{this.results.total}}
-        </p>
-        <p v-bind:class="{'show-results': showImages}">
-          <button v-on:click="pageBack">  previous page </button>
+    <div 
+      class ='results-container' 
+      v-bind:class="{'hidden': showImages=== false}"
+    >
+      <p>
+        total images matching keyword: {{this.results.total}}
+      </p>
+      <p v-bind:class="{'show-results': showImages}">
+        <button class="page-back-btn" v-on:click="pageBack">  
+          <font-awesome-icon icon="arrow-left" class="search-icon" />
+        </button>
           page {{this.page}} out of {{this.results.pages}}
-          <button v-on:click="pageForward">  next page </button>
-        </p>
+        <button class="page-next-btn" v-on:click="pageForward">  
+          <font-awesome-icon icon="arrow-right" class="search-icon" />
+        </button>
+      </p>
     </div>
       <Loading v-if="loading" />
-      <Photos v-else :images="images" :results="results" :showImages="showImages"/>
+      <Photos v-else 
+        :images="images" 
+        :results="results" 
+        :showImages="showImages"
+      />
   </div>
 </template>
 
@@ -95,19 +108,18 @@ export default {
 
 
 #app {
-  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
-  font-family: 'Quicksand', sans-serif;
-  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  -webkit-font-smoothing: antialiased;
   align-items: center;
+  background-color: #2c3e50;
+  color: white;
   display: flex;
   flex-direction: column;
-  background-color: #2c3e50;
+  font-family: 'Quicksand', sans-serif;
+  height: 102vh;
   margin-top: -10px;
   padding: 0;
-  color: white;
-  height: 102vh;
+  text-align: center;
 }
 
 h1 {
@@ -122,10 +134,6 @@ form {
   width: 90vw;
 }
 
-.results-container {
-
-}
-
 .hidden {
   display: none
 }
@@ -136,28 +144,25 @@ form {
 }
 
 .search-icon {
-  /* position: relative; */
-  /* right: 10px; */
   font-size: 32px;
 }
 
 input {
+  background-color: unset;
+  border: none;
+  color: white;
   font-family: 'Quicksand', sans-serif;
   font-size: 40px;
-  border: none;
-  background-color: unset;
   margin: 20px;
-  color: white;
-
 }
 button {
+  background-color: #2c3e50;
+  border-radius: 4px;
+  border: solid white 1px;  
+  color: white;
   font-family: 'Quicksand', sans-serif;
   font-size: 32px;
   margin-left: 20px;
-  color: white;
-  background-color: #2c3e50;
-  border: solid white 1px;  
-  border-radius: 4px;
 };
 
 button:hover {
--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,35 +1,35 @@
 <template>
-<div>
-  <div class="image-container" v-if="images.length > 0" 
-  >
-    <div class="card" v-for ='image in images' v-bind:key='image.id' 
-    @mouseover="shownCard = image.id"
-    @mouseleave="shownCard = null" 
-    v-bind:style="{ backgroundImage: 'url('+image.image+')' }">  
-       <p class="description" v-if="shownCard === image.id"> {{image.description}} </p>
+  <div>
+    <div class="image-container" v-if="images.length > 0" 
+    >
+      <div class="card" 
+        v-for ='image in images' 
+        v-bind:key='image.id' 
+        @mouseover="shownCard = image.id"
+        @mouseleave="shownCard = null" 
+        v-bind:style="{ backgroundImage: 'url('+image.image+')' }">  
+        <p class="description" 
+          v-if="shownCard === image.id"> 
+          {{image.description}} 
+        </p>
       </div> 
-  </div>
-  <div v-else-if="showImages">
-    <p>
-      no images matched your search!
+    </div>
+    <div v-else-if="showImages">
+      <p>
+        no images matched your search!
       </p>
     </div>
-    </div>
+  </div>
 </template>
 
 
 <script>
 export default {
   name: 'Photos',
-  data(){
+  data() {
     return {
       hoverState: false,
       shownCard: null
-    }
-  },
-  methods: {
-    hoverImage (id) {
-      this.active = !this.active
     }
   },
   props: {
@@ -46,27 +46,27 @@ export default {
 input {
   font-size: 32px;
 }
+
 .image-container {
-  /* display: grid;
-  grid-template-columns: repeat( auto-fit, minmax(250px, 1fr) ); */
   background-color: #2c3e50;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
 }
 .card {
-  display: flex;
-  width: 400px;
-  height: 400px;
+  align-items: center;
   background-position: center; 
   background-size: cover;
   border-radius: 20px;
+  display: flex;
   flex-direction: column;
+  height: 400px;
   justify-content: flex-end;
-  align-items: center;
-  padding: 4px;
   margin: 16px;
+  padding: 4px;
+  width: 400px;
 }
+
 button {
   font-size: 32px;
   margin-left: 20px
